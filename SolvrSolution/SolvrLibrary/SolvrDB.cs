7e0f47a33f85517a4f1d4359c19173ca5ca11120
David Christopher Hutchinson <1061883@ucn.dk>
2017-11-16 13:56:48 +0100

    Har tilføjet kommentar til solvrDB klassen

--- SolvrSolution/SolvrLibrary/SolvrDB.cs
+++ SolvrSolution/SolvrLibrary/SolvrDB.cs
@@ -9,6 +9,7 @@ namespace SolvrLibrary
 {
     class SolvrDB : DataContext
     {
+        //Here is the tables for the project. These tables are in the database.
         public Table<Post> Posts;
         public Table<User> Users;
         public Table<Comment> Comments;
@@ -18,6 +19,11 @@ namespace SolvrLibrary
         public Table<Vote> Votes;
         public Table<PhysicalPost> PhysicalPosts;
 
+        /// <summary>
+        /// Used to create a connection to the database through a connection string.
+        /// Base is an inheritance that createse a new instance of the datacontext class (In here SolvrDB)
+        /// </summary>
+        /// <param name="connection"></param>
         public SolvrDB(string connection) : base(connection){ }
     }
 }
