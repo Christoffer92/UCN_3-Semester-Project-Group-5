1edbb381e94bf3c17f67e017b7ad808a89d63b97
Anders <31887171+Sruffen@users.noreply.github.com>
2017-12-13 13:51:16 +0100

    Fixed Html Injections... Again

--- SolvrSolution/SolvrWebClient/Models/CreatePostViewModels.cs
+++ SolvrSolution/SolvrWebClient/Models/CreatePostViewModels.cs
@@ -3,23 +3,28 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace SolvrWebClient.Models
 {
     public class PostViewModel
     {
         [Required]
+        [AllowHtml]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
         public string Title { get; set; }
 
         [Required]
+        [AllowHtml]
         [Display(Name = "Category")]
         public int CategoryId { get; set; }
 
         [Required]
+        [AllowHtml]
         [StringLength(1500, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 5)]
         public string Description { get; set; }
 
+        [AllowHtml]
         [StringLength(300)]
         public string TagsString { get; set; }
 
@@ -29,27 +34,34 @@ namespace SolvrWebClient.Models
     public class PhysicalPostViewModel
     {
         [Required]
+        [AllowHtml]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
         public string Title { get; set; }
 
         [Required]
+        [AllowHtml]
         [Display(Name = "Category")]
         public int CategoryId { get; set; }
 
         [Required]
+        [AllowHtml]
         [StringLength(1500, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 5)]
         public string Description { get; set; }
 
+        [AllowHtml]
         [StringLength(300)]
         public string TagsString { get; set; }
-        
+
+        [AllowHtml]
         [StringLength(1000, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 5)]
         public string AltDescription { get; set; }
 
         [Required]
+        [AllowHtml]
         [StringLength(10, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
         public string Zipcode { get; set; }
-        
+
+        [AllowHtml]
         [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
         public string Address { get; set; }
 
