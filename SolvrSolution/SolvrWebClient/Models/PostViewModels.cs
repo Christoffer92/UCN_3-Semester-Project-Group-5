f7ce409f14ce7f13e5a7741790d7b349ea318de1
Anders <31887171+Sruffen@users.noreply.github.com>
2017-12-13 14:24:18 +0100

    Fixed it again a special

--- SolvrSolution/SolvrWebClient/Models/PostViewModels.cs
+++ SolvrSolution/SolvrWebClient/Models/PostViewModels.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace SolvrWebClient.Models
 {
     public class CommentViewModel
     {
         [Required]
+        [AllowHtml]
         [StringLength(1500, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 1)]
         public string Text { get; set; }
 
